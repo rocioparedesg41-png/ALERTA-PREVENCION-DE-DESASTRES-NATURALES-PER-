194dad51feb6104b9378df6d8bfdd6861747c044
Rocío Paredes Gamarra <rocioparedesg41@gmail.com>
2026-09-17 12:17:56 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,3 +13,10 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('SW registrado:', reg.scope))
+      .catch(err => console.error('SW error:', err));
+  });
+}
